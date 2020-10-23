ddc61ac858323ac00b104d346675670e83b60933
IvoTsochev <ivo.tsochev@live.com>
2020-10-24 00:19:39 +0300

    styling

--- src/Infobox.jsx
+++ src/Infobox.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
-function Infobox({title, cases, total}) {
-    return (
-        <Card className='infoBox'>
-            <CardContent>
-                {/* Title */}
-                <Typography className='infoBox__title' color='textSecondary'>
-                    {title}
-                </Typography>
+function Infobox({ title, cases, total }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        {/* Title */}
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
 
-                {/* Number of cases */}
+        {/* Number of cases */}
 
-                <h2 className='infoBox__cases'>
-                    {cases}
-                </h2>
+        <h2 className="infoBox__cases">{cases}</h2>
 
-                {/* Total */}
-                <Typography className='infoBox__total' color='textSecondary'>
-                    {total} Total
-                </Typography>
-            </CardContent>
-        </Card>
-    )
+        {/* Total */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default Infobox
+export default Infobox;
